7ebc8da99c9181a4cf46b7e180277b0521181932
tonynap <tonynap@vt.edu>
2018-10-25 21:00:02 -0400

    have 3 rooms with doors and teleportation

---
+++ Assets/TeleportToObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToObject : MonoBehaviour {
+
+	[Tooltip("Teleport to the location of this object")]
+	public GameObject teleportDestinationObject;
+
+	// Use this for initialization
+	void Start () {
+			Debug.Log("Started game.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+			Debug.Log("Player entered trigger");
+			other.gameObject.transform.position = teleportDestinationObject.transform.position;
+	}
+
+}
