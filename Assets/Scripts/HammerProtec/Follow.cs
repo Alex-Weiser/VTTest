6149cda51721691050c16fd2a920568a70f003d9
Alex Weiser <aweiser2@gmu.edu>
2018-12-10 15:03:44 -0500

    hammer scene, changed map and started AI

---
+++ Assets/Scripts/HammerProtec/Follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Follow : MonoBehaviour {
+    public NavMeshAgent myNav;
+    public Transform Player;
+	// Use this for initialization
+	void Start () {
+        myNav = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag ("Target").GetComponent<Transform>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Target").GetComponent<Transform>();
+        }
+            // gameObject.transform.rotation = new Quaternion(0, 0, 0,0); 
+            //transform.LookAt(Player.transform.position);
+            myNav.SetDestination(Player.transform.position);
+        
+        
+	}
+}
